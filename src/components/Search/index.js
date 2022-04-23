03c2a8ee74f0030e71a2029a4939aec1ba94b9d2
Sameer <sameerwork2248@gmail.com>
2022-04-23 22:08:42 +0530

    indent

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -16,36 +16,35 @@ const Search = () => {
 
     const searchWord = async () => {
         try {
-            const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`,{});
-            if(data.status === 200) {
-                setDefiArray(data.data.splice(0,1));
+            const data = await axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchedWord}`, {});
+            if (data.status === 200) {
+                setDefiArray(data.data.splice(0, 1));
             } else {
                 throw new Error(data.title);
             }
         } catch (error) {
             if (error.message == 'Request failed with status code 404') {
-                addToast('No defination found.', { appearance: 'error', autoDismiss:true });
+                addToast('No defination found.', { appearance: 'error', autoDismiss: true });
             } else {
-                addToast(error.message || 'Something went wrong', { appearance: 'error', autoDismiss:true });
+                addToast(error.message || 'Something went wrong', { appearance: 'error', autoDismiss: true });
             }
             setDefiArray([]);
         }
     }
 
     const handleKeyPress = (e) => {
-        if(e.key === 'Enter' && searchedWord != ''){
+        if (e.key === 'Enter' && searchedWord != '') {
             searchWord();
         }
     }
-    console.log('asidfansuf',defiArray)
     return (
         <>
             <div className='mb-4' style={{ display: 'flex' }}>
                 <InputGroup>
-                    <Input bsSize='lg' onChange={(e) => handleChange(e.target.value)} id='word' name ='word'
-                    onKeyDown={(e) => handleKeyPress(e) }/>
+                    <Input bsSize='lg' onChange={(e) => handleChange(e.target.value)} id='word' name='word'
+                        onKeyDown={(e) => handleKeyPress(e)} />
                     <Button onClick={() => searchWord()} color="success"
-                    disabled={ searchedWord != '' ? false : true}>
+                        disabled={searchedWord != '' ? false : true}>
                         Search
                     </Button>
                 </InputGroup>
@@ -53,13 +52,13 @@ const Search = () => {
             <div style={{ maxHeight: '400px', overflow: 'auto', overflowX: 'hidden' }}>
                 <Row style={{ justifyContent: 'center' }}>
                     {defiArray.length > 0 && defiArray.map((data, i) =>
-                        <Col md='8' key={data.word + i}> 
+                        <Col md='8' key={data.word + i}>
                             <List data={data} index={i} key={data.word + i + 'list'} />
                         </Col>
                     )}
                 </Row>
             </div>
-            
+
         </>
     )
 }
