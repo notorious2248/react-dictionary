31cfa6f8ab2ddcf6ca77f7d26f5fc45879933cef
Sameer <sameerwork2248@gmail.com>
2022-04-23 22:00:36 +0530

    commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Search from './components/Search'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Search />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/List/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const List = (props) => {
+    let { data, index } = props;
+    console.log('asdojasd',data)
+    return (
+            <div key={data.word  + index + 'inlist'}>
+                <ul class="list-unstyled">
+                    {data.meanings[0].definitions.map(d => {
+                        return <li key={d.definitions} class="list-group-item li-tag">{d.definition}</li>
+                    })}
+                </ul>
+            </div>
+    )
+}
+
+export default List;
